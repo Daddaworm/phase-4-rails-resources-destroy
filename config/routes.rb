6cc738ddb3ad93fe3f4dd0b6f24cf959575d7ec0
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-30 11:23:35 -0400

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  # resources :birds, only: [:index, :show, :create, :update, :destroy] 
+  resources :birds #since using all 5 CRUD no need for the "only" code above.
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
