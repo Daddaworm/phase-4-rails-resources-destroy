2527cbc09a6be699445232ec8918daec0e8079ef
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-30 11:23:13 -0400

    commit tree c62b1290f9ccdd383736a409b5df12b862cd48f7

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,16 +1,19 @@
 class BirdsController < ApplicationController
 
+
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
 
+
   # POST /birds
   def create
     bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
+
 
   # GET /birds/:id
   def show
@@ -21,6 +24,7 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
 
   # PATCH /birds/:id
   def update
@@ -33,6 +37,19 @@ class BirdsController < ApplicationController
     end
   end
 
+
+  #DESTROY /bird/:id
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+
   # PATCH /birds/:id/like
   def increment_likes
     bird = Bird.find_by(id: params[:id])
@@ -43,6 +60,7 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
 
   private
 
